51c9acc5b28a9d916fc4a08f8872121e7a60ebf4
shiva <kshivayadav7@gmail.com>
2025-12-16 23:11:43 +0530

    folders added

---
+++ template.sh
@@ -0,0 +1,13 @@
+mkdir -p src
+mkdir -p research
+
+touch src/helper.py
+touch src/prompt.py
+touch src/__init__.py
+touch research/medical_chatbot.ipynb
+touch setup.py
+touch app.py
+touch .env
+touch requirements.txt
+
+echo "Directory structure & files created successfully."
